7a224ed8ead80485d801124bef24d6cad0fc15b0
Silvio Barbotto <silvio.barbotto@edu.unito.it>
2018-01-04 19:53:06 +0100

    try /upload route

--- routes/upload.js
+++ routes/upload.js
@@ -5,17 +5,7 @@ var router = express.Router();
 
 router.get('/upload', function(req, res, next) {
   
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  
-  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-  
-  res.write('<input type="file" name="filetoupload"><br>');
-  
-  res.write('<input type="submit">');
-  
-  res.write('</form>');
-  
-  return res.end();
+  res.send('respond with a resource');
   
 });
 
