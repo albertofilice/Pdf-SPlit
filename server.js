3702c9a07b9e68d8ab715c8e89a172d5d0b71861
Silvio Barbotto <silvio.barbotto@edu.unito.it>
2018-01-08 19:42:52 +0100

    add verbose output

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const app = require('./app');
 const http = require('http');
 
 http.createServer(app).listen(process.env.PORT);
+
+if(process.env.NODE_ENV !== 'production') {
+  process.once('uncaughtException', function(err) {
+    console.error('FATAL: Uncaught exception.');
+    console.error(err.stack||err);
+    setTimeout(function(){
+      process.exit(1);
+    }, 100);
+  });
+}
