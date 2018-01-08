5759b49e289a11451118d8a2c204987e7964bf61
Silvio Barbotto <silvio.barbotto@edu.unito.it>
2018-01-08 18:37:44 +0100

    try to send attachment 7

--- email-sender.js
+++ email-sender.js
@@ -25,6 +25,15 @@ module.exports = function(to, attachments, callback){
     )
   );
   
+  // verify connection configuration
+  transporter.verify(function(error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Server is ready to take our messages');
+    }
+  });
+  
   transporter.sendMail({
     
     from: 'wdltest@gmail.com',
