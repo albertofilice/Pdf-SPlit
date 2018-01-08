e56cc6bf08cd7ccd9e587cc7091ec00e0ac85770
Silvio Barbotto <silvio.barbotto@edu.unito.it>
2018-01-08 11:23:50 +0100

    the file!

---
+++ db-mysql.js
@@ -0,0 +1,55 @@
+var mysql = require('mysql');
+
+module.exports = function(query){
+    
+  var connection = mysql.createConnection(
+    {
+      host     : 'localhost:3306',
+      user     : 'test',
+      password : 'test_123',
+      database : 'test',
+    }
+  );
+  
+  connection.connect();
+  
+  var queryString = 'SELECT * FROM users';
+  
+  connection.query(queryString);
+
+  
+  
+  query.on('error', function(err) {
+    
+    throw err;
+
+  });
+  
+  
+  
+  query.on('fields', function(fields) {
+  
+    console.log(fields);
+  
+  });
+  
+  
+  
+  query.on('result', function(row) {
+    
+    connection.pause();
+    
+    //do cose
+  
+    console.log(row);
+    
+    connection.resume();
+
+  });
+  
+
+
+  connection.end();
+  
+}
+
